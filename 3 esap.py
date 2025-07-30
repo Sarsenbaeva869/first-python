8f9bd74ff87d2ef8c3eefe9469181d84b11e84dc
Sarsenbaeva869 <sarsenbaevagulmira50@gmail.com>
2025-07-30 09:57:35 +0500

    Add files via upload

---
+++ 3 esap.py
@@ -0,0 +1,10 @@
+a = int(input("birinshi sandi kirit:"))
+b= int(input("ekinshi sandi kiritin:"))
+c = int(input("ushinshi nsandin kiritin:"))
+if a<= b and a<= c:
+    kishkentayi= a
+elif b<= a and b<= c:
+    kishkentayi = b
+else: 
+    kishkentayi= c 
+print("kishkentayi", kishkentayi)
